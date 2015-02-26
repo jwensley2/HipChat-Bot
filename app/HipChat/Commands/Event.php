5d8a750db64541d399c8833c0288c9b7f2f0ba17
Joseph Wensley <jwensley2@gmail.com>
2015-02-26 00:06:59 -0500

    Show event ID in list
    Sort event list and only show future events
    Add delete sub command
    Add some checks and throw exceptions when appropriate

--- app/HipChat/Commands/Event.php
+++ app/HipChat/Commands/Event.php
@@ -25,20 +25,43 @@ class Event extends AbstractCommand implements CommandInterface
         $roomId = $hookEvent->item->room->id;
         $subCommand = $this->getSubCommand($command);
 
+        $commandMessage = trim(preg_replace("/^{$subCommand}/i", '', $command->getMessage()));
+
         switch ($subCommand) {
             case 'create':
             case 'add':
             case 'new':
-                $event = $this->createEvent($command, $hookEvent);
-                $this->sendMessage($roomId, 'Event created');
+                try {
+                    $this->createEvent($commandMessage, $hookEvent);
+                    $this->sendMessage($roomId, 'Event created');
+                } catch (\Exception$e) {
+                    $this->sendMessage($roomId, $e->getMessage());
+                }
                 break;
             case 'list':
-                $events = $this->getRoomEvents($hookEvent);
+                $events = $this->getRoomEvents($hookEvent->item->room->id, ['after' => new \DateTime()]);
                 $this->sendMessage($roomId, view('hipchat.commands.eventlist')->with(['events' => $events])->render());
+                break;
+            case 'delete':
+                try {
+                    $this->deleteEvent($commandMessage, $hookEvent);
+                    $this->sendMessage($roomId, 'Event deleted');
+                } catch (\Exception$e) {
+                    $this->sendMessage($roomId, $e->getMessage());
+                }
+                break;
+            default:
+                $this->sendMessage($roomId, 'Invalid Command');
                 break;
         }
     }
 
+    /**
+     * Get the sub command
+     *
+     * @param \App\HipChat\CommandParser $command
+     * @return mixed|null
+     */
     protected function getSubCommand(CommandParser $command)
     {
         $subCommand = current(explode(' ', $command->getMessage()));
@@ -50,19 +73,32 @@ class Event extends AbstractCommand implements CommandInterface
         return null;
     }
 
-    protected function createEvent(CommandParser $command, RoomMessage $hookEvent)
+    /**
+     * Create a new event
+     *
+     * @param string      $commandMessage
+     * @param RoomMessage $hookEvent
+     * @return \App\Event
+     * @throws \Exception
+     */
+    protected function createEvent($commandMessage, RoomMessage $hookEvent)
     {
-        $regex = '/{([a-zA-Z0-9\-:\s]*)}/';
-
-        // Get the message without the sub command
-        $message = implode(' ', array_slice(explode(' ', $command->getMessage()), 1));
+        $regex = '/^{([a-zA-Z0-9\-:\s]*)}/';
 
         // Get the date
-        preg_match($regex, $message, $date);
-        $date = $date[1];
+        preg_match($regex, $commandMessage, $date);
+        $date = isset($date[1]) ? $date[1] : null;
+
+        if (!$date) {
+            throw new \Exception('No date found');
+        }
 
         // Strip the date
-        $description = preg_replace($regex, '', $message, 1);
+        $description = trim(preg_replace($regex, '', $commandMessage, 1));
+
+        if (empty($description)) {
+            throw new \Exception('You must provide a description');
+        }
 
         // Save the event
         $event = new \App\Event();
@@ -76,10 +112,53 @@ class Event extends AbstractCommand implements CommandInterface
         return $event;
     }
 
-    protected function getRoomEvents(RoomMessage $hookEvent)
+    /**
+     * Delete an event
+     *
+     * @param int|string  $id
+     * @param RoomMessage $hookEvent
+     * @throws \Exception
+     */
+    protected function deleteEvent($id, RoomMessage $hookEvent)
     {
-        $events = \App\Event::whereRoomId($hookEvent->item->room->id)->get();
+        if (!is_numeric($id)) {
+            throw new \Exception('ID must be a number');
+        }
 
-        return $events;
+        /** @var \App\Event $event */
+        $event = \App\Event::find($id);
+
+        if (!$event) {
+            throw new \Exception('Event not found');
+        }
+
+        if ($event->creator_id !== $hookEvent->item->message->from->id) {
+            throw new \Exception('You cannot delete another user\'s event');
+        }
+
+        $event->delete();
+    }
+
+    /**
+     * Get the events for a room
+     *
+     * @param int   $roomId
+     * @param array $options
+     * @return mixed
+     */
+    protected function getRoomEvents($roomId, array $options = null)
+    {
+        $events = \App\Event::whereRoomId($roomId)
+            ->orderBy('date', 'desc');
+
+        if (isset($options['after'])) {
+            $events->where('date', '>=', $options['after']);
+        }
+
+        if (isset($options['before'])) {
+            $events->where('date', '<=', $options['before']);
+        }
+
+        return $events->get();
     }
 }
--- resources/views/hipchat/commands/eventlist.php
+++ resources/views/hipchat/commands/eventlist.php
@@ -2,9 +2,9 @@
 /** @var \App\Event[] $events */
 ?>
 
-<strong>Events</strong><br>
+<strong>Upcoming Events</strong><br>
 <?php foreach ($events as $event): ?>
     <p>
-        <strong><?= $event->date->format('Y-m-d H:i') ?></strong> - <?= $event->description ?>
+        {<?= $event->id ?>} <strong><?= $event->date->format('Y-m-d H:i') ?></strong> - <?= $event->description ?>
     </p>
 <?php endforeach; ?>
